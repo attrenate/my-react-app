c7e5a6807e55058bcc3dd96f321f8cfce5206163
attrenate <nikolozghomidzevol100@gmail.com>
2025-05-18 17:49:09 +0400

    intoduction to react

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,21 @@
+import Header from './Header.jsx'
+import footer from './footer.jsx'
+import Food from './Food.jsx' 
+
+function App() {
+  
+  return(
+   <>
+   <header>
+      
+    </header>
+    <Food/>
+    <Food />
+    <footer>
+
+    </footer>
+   </>
+  );
+}
+
+export default App
---
+++ vite-project/src/footer.jsx
@@ -0,0 +1,10 @@
+function footer(){
+    return(
+        <footer>
+            <p>
+                &copy; {new Date().getFullYear()}Your w3ebsite name
+            </p>
+        </footer>
+    );
+}
+export default footer
